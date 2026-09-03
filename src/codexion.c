9c6ceaf03ba0aaef29129055a93714657e4c7cad
Jay Krishnan <jaykrishnan1997@gmail.com>
2026-09-03 19:18:53 +0200

    a start main

---
+++ src/codexion.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jay-k <jay-k@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/03 15:51:15 by jay-k             #+#    #+#             */
+/*   Updated: 2026/09/03 19:17:15 by jay-k            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <codexion.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+
+
+int main(int argc, char **argv){
+
+    (void)argc;
+    (void)argv;
+
+    return 0;
+}
