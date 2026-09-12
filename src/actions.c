184c6e73a77876d67ffd17e3acccfd2dafde9636
Jay Krishnan <jaykrishnan1997@gmail.com>
2026-09-12 19:48:25 +0200

    norm fix from action(incl.)

--- src/actions.c
+++ src/actions.c
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   actions.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkrishna <jkrishna@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jay-k <jay-k@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 11:03:36 by jkrishna          #+#    #+#             */
-/*   Updated: 2026/09/12 11:37:51 by jkrishna         ###   ########.fr       */
+/*   Updated: 2026/09/12 19:47:59 by jay-k            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
 static int	take_one_dongle(t_coder *coder, t_dongle *dongle) {
-	
-	t_heap_node request;
+
+	t_heap_node	request;
 	struct timespec ts;
+
 	request.coder = coder;
 	pthread_mutex_lock(&coder->data->sequence_mutex);
 	request.sequence = coder->data->next_sequence++;
@@ -29,21 +30,21 @@ static int	take_one_dongle(t_coder *coder, t_dongle *dongle) {
 		|| get_time() < dongle->available_at
 		|| dongle->request_heap.size == 0
 		|| dongle->request_heap.nodes[0].coder != coder)
+	{
+		if (is_simulation_over(coder->data))
 		{
-			if (is_simulation_over(coder->data))
-			{
-				pthread_mutex_unlock(&dongle->mutex);
-				return (-1);
-			}
-			clock_gettime(CLOCK_REALTIME, &ts);
-			ts.tv_nsec += 5000000;
-			if (ts.tv_nsec >= 1000000000)
-			{
-				ts.tv_sec += 1;
-				ts.tv_nsec -= 1000000000;
-			}
-			pthread_cond_timedwait(&dongle->cond, &dongle->mutex, &ts);
+			pthread_mutex_unlock(&dongle->mutex);
+			return (-1);
 		}
+		clock_gettime(CLOCK_REALTIME, &ts);
+		ts.tv_nsec += 5000000;
+		if (ts.tv_nsec >= 1000000000)
+		{
+			ts.tv_sec += 1;
+			ts.tv_nsec -= 1000000000;
+		}
+		pthread_cond_timedwait(&dongle->cond, &dongle->mutex, &ts);
+	}
 	heap_extract_min(&dongle->request_heap, coder->data->scheduler);
 	dongle->in_use = 1;
 	pthread_mutex_unlock(&dongle->mutex);
@@ -83,8 +84,8 @@ int	take_dongles(t_coder *coder)
 	return (0);
 }
 
-static void	release_one_dongles(t_coder *coder, t_dongle *dongle) {
-
+static void	release_one_dongles(t_coder *coder, t_dongle *dongle)
+{
 	pthread_mutex_lock(&dongle->mutex);
 	dongle->available_at = get_time() + coder->data->dongle_cooldown;
 	dongle->in_use = 0;
@@ -92,12 +93,12 @@ static void	release_one_dongles(t_coder *coder, t_dongle *dongle) {
 	pthread_mutex_unlock(&dongle->mutex);
 }
 
-void release_dongles(t_coder *coder)
+void	release_dongles(t_coder *coder)
 {
 	if (coder->left_dongle == coder->right_dongle)
 	{
 		release_one_dongles(coder, coder->left_dongle);
-		return;
+		return ;
 	}
 	release_one_dongles(coder, coder->left_dongle);
 	release_one_dongles(coder, coder->right_dongle);
