dfb53063a0ffddcb6fc744c5009a4d461f6b64e2
Jay Krishnan <jkrishna@student.42heilbronn.de>
2026-09-14 09:59:00 +0200

    norm fix on heap, init and codexion.c

--- src/heap.c
+++ src/heap.c
@@ -6,13 +6,13 @@
 /*   By: jkrishna <jkrishna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 13:18:46 by jkrishna          #+#    #+#             */
-/*   Updated: 2026/09/12 11:36:43 by jkrishna         ###   ########.fr       */
+/*   Updated: 2026/09/14 09:39:48 by jkrishna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-static int is_smaller(t_heap_node a, t_heap_node b, char *scheduler)
+static int	is_smaller(t_heap_node a, t_heap_node b, char *scheduler)
 {
 	if (strcmp(scheduler, "fifo") == 0)
 		return (a.sequence < b.sequence);
@@ -20,17 +20,17 @@ static int is_smaller(t_heap_node a, t_heap_node b, char *scheduler)
 		return (a.deadline < b.deadline);
 }
 
-void	heap_insert(t_heap *heap, t_heap_node new_node, char *scheduler) {
-    int			i;
-	int 		parent;
+void	heap_insert(t_heap *heap, t_heap_node new_node, char *scheduler)
+{
+	int			i;
+	int			parent;
 	t_heap_node	swap;
 
-	// add the new node at the end then sort them from min to max
 	heap->nodes[heap->size] = new_node;
 	i = heap->size;
 	heap->size++;
-
-	while (i > 0) {
+	while (i > 0)
+	{
 		parent = (i - 1) / 2;
 		if (is_smaller(heap->nodes[i], heap->nodes[parent], scheduler))
 		{
@@ -40,37 +40,46 @@ void	heap_insert(t_heap *heap, t_heap_node new_node, char *scheduler) {
 			i = parent;
 		}
 		else
-			break;
+			break ;
 	}
 }
 
-t_heap_node	heap_extract_min(t_heap *heap, char *scheduler) {
+static void	sift_down(t_heap *heap, char *scheduler)
+{
 	int			i;
 	int			left;
 	int			right;
 	int			smallest;
-	t_heap_node	min;
 	t_heap_node	temp;
-	
-	min = heap->nodes[0];
-	heap->size--;
-	heap->nodes[0] = heap->nodes[heap->size];
+
 	i = 0;
-	while (1) {
+	while (1)
+	{
 		left = 2 * i + 1;
 		right = 2 * i + 2;
 		smallest = i;
-		
-		if (left < heap->size && is_smaller(heap->nodes[left], heap->nodes[smallest], scheduler))
+		if (left < heap->size
+			&& is_smaller(heap->nodes[left], heap->nodes[smallest], scheduler))
 			smallest = left;
-		if (right < heap->size && is_smaller(heap->nodes[right], heap->nodes[smallest], scheduler))
+		if (right < heap->size
+			&& is_smaller(heap->nodes[right], heap->nodes[smallest], scheduler))
 			smallest = right;
 		if (smallest == i)
-			break;
+			break ;
 		temp = heap->nodes[i];
 		heap->nodes[i] = heap->nodes[smallest];
 		heap->nodes[smallest] = temp;
 		i = smallest;
 	}
+}
+
+t_heap_node	heap_extract_min(t_heap *heap, char *scheduler)
+{
+	t_heap_node	min;
+
+	min = heap->nodes[0];
+	heap->size--;
+	heap->nodes[0] = heap->nodes[heap->size];
+	sift_down(heap, scheduler);
 	return (min);
 }
--- src/init.c
+++ src/init.c
@@ -6,129 +6,127 @@
 /*   By: jkrishna <jkrishna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 11:19:56 by jkrishna          #+#    #+#             */
-/*   Updated: 2026/09/12 12:50:26 by jkrishna         ###   ########.fr       */
+/*   Updated: 2026/09/14 09:58:33 by jkrishna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-static int  is_valid_number(const char *str){
-    int i;
-
-    i = 0;
-    if (str[i] == '\0')
-        return (0);
-    if (str[i] == '+')
-        i++;
-    if (str[i] == '\0')
-        return (0);
-    while (str[i]) 
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
-}
-
-int parser(int argc, char **argv, t_data *data)
+static int	is_valid_number(const char *str)
 {
+	int	i;
 
-    if (argc != 9)
-        return (-1);
-    int i;
-
-    i = 1;
-    while (i <= 7)
-    {
-        if (!is_valid_number(argv[i]))
-        {
-            printf("Argument %d must be a positive integer\n", i);
-            return (-1);
-        }
-
-        if (atoi(argv[i]) <= 0 && (i != 7))
-        {
-            printf("Argument %d must be greater than zero\n", i);
-            return (-1);
-        }
-        if (atoi(argv[i]) < 0 && (i == 7))
-        {
-            printf("Argument %d must be greater than or equal to zero\n", i);
-            return (-1);
-        }
-        i++;
-    }
-    if (strcmp(argv[8], "fifo") != 0 && strcmp(argv[8], "edf") != 0) {
-        
-        printf("Scheduler must be exactly 'fifo' or 'edf'\n");
-        return (-1);
-    }
-
-    // consuming parameters for data
-    // printf("./codexion.c nc tb tc td tr ncr dc sch")
-    data->num_coders = atoi(argv[1]); // nc
-    data->time_to_burnout = atoi(argv[2]); // tb
-    data->time_to_compile = atoi(argv[3]); // tc
-    data->time_to_debug = atoi(argv[4]); // td
-    data->time_to_refractor = atoi(argv[5]); // tr
-    data->required_compiles = atoi(argv[6]); // ncr
-    data->dongle_cooldown = atoi(argv[7]); // dc
-    data->scheduler = argv[8]; // sch
-    
-    return (0);
+	i = 0;
+	if (str[i] == '\0')
+		return (0);
+	if (str[i] == '+')
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-t_dongle	*init_dongles(t_data *data) {
-    int			d;
-    // list of pointers with address to dongles
-    t_dongle 	*dongles;
-    
+static int	validate_numbers(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc != 9)
+		return (-1);
+	while (i <= 7)
+	{
+		if (!is_valid_number(argv[i]))
+		{
+			printf("Argument %d must be a positive integer\n", i);
+			return (-1);
+		}
+		if (atoi(argv[i]) <= 0 && (i != 7))
+		{
+			printf("Argument %d must be greater than zero\n", i);
+			return (-1);
+		}
+		if (atoi(argv[i]) < 0 && (i == 7))
+		{
+			printf("Argument %d must be greater than or equal to zero\n", i);
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	parser(int argc, char **argv, t_data *data)
+{
+	if (validate_numbers(argc, argv) == -1)
+		return (-1);
+	if (strcmp(argv[8], "fifo") != 0 && strcmp(argv[8], "edf") != 0)
+	{
+		printf("Scheduler must be exactly 'fifo' or 'edf'\n");
+		return (-1);
+	}
+	data->num_coders = atoi(argv[1]);
+	data->time_to_burnout = atoi(argv[2]);
+	data->time_to_compile = atoi(argv[3]);
+	data->time_to_debug = atoi(argv[4]);
+	data->time_to_refractor = atoi(argv[5]);
+	data->required_compiles = atoi(argv[6]);
+	data->dongle_cooldown = atoi(argv[7]);
+	data->scheduler = argv[8];
+	return (0);
+}
+
+t_dongle	*init_dongles(t_data *data)
+{
+	int			d;
+	t_dongle	*dongles;
+
 	dongles = malloc(sizeof(t_dongle) * data->num_coders);
 	if (!dongles)
 		return (NULL);
-    d = 0;
-
-    while (d < data->num_coders)
-    {
-        dongles[d].dongle_id = d;
+	d = 0;
+	while (d < data->num_coders)
+	{
+		dongles[d].dongle_id = d;
 		pthread_mutex_init(&dongles[d].mutex, NULL);
-    	pthread_cond_init(&dongles[d].cond, NULL);
+		pthread_cond_init(&dongles[d].cond, NULL);
 		dongles[d].in_use = 0;
 		dongles[d].available_at = 0;
-        // intitalizing the request part per dongle
-        dongles[d].request_heap.nodes = malloc(sizeof(t_heap_node) * data->num_coders);
-        // THIS IS NOT ENOUGH INCASE SOME OTHER MEMORY WAS
-        // ALREADY ALLOCATED
-        if (!dongles[d].request_heap.nodes)
-            return (NULL);
-        dongles[d].request_heap.size = 0;
-        dongles[d].request_heap.capacity = data->num_coders;
-        
+		dongles[d].request_heap.nodes = malloc(
+				sizeof(t_heap_node) * data->num_coders);
+		if (!dongles[d].request_heap.nodes)
+			return (NULL);
+		dongles[d].request_heap.size = 0;
+		dongles[d].request_heap.capacity = data->num_coders;
 		d++;
-    }
-    return (dongles);
+	}
+	return (dongles);
 }
 
-int	init_coders(t_data *data, t_dongle *dongles) {
-    int c;
-    // list of pointers with address to dongles
-	
+int	init_coders(t_data *data, t_dongle *dongles)
+{
+	int	c;
+
 	data->coders = malloc(sizeof(t_coder) * data->num_coders);
 	if (!data->coders)
 		return (-1);
-    c = 0;
-    while (c < data->num_coders)
-    {
-        data->coders[c].coder_id = c;
+	c = 0;
+	while (c < data->num_coders)
+	{
+		data->coders[c].coder_id = c;
 		data->coders[c].no_of_compiles = 0;
 		data->coders[c].current_state = "idle";
 		data->coders[c].left_dongle = &dongles[c];
 		data->coders[c].right_dongle = &dongles[(c + 1) % data->num_coders];
 		data->coders[c].data = data;
 		data->coders[c].last_compile_start = 0;
-        pthread_mutex_init(&data->coders[c].state_mutex, NULL);
+		pthread_mutex_init(&data->coders[c].state_mutex, NULL);
 		c++;
-    }
-    return (0);
+	}
+	return (0);
 }
